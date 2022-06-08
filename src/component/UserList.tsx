83301e1316c24f13decc5057be5d02fce7272b6e
kangchangu <lyhand@naver.com>
2022-06-08 14:00:38 +0900

    userCallback 테스트중

--- src/component/UserList.tsx
+++ src/component/UserList.tsx
@@ -1,55 +1,142 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 
 interface UserModel {
     id : number ;
     username ? : string;
     email : string;
+    active : boolean;
 }
-
-const users  : UserModel[] = [
-    {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com'
-      },
-      {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com'
-      },
-      {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
-      }
-    ];
 
     
 const UserList = () => {
+    const nextId = useRef(4);
+    type test = string[];
+    type test2 = number[];
 
-  
+    let testdata : test & test2 = [];
+
+    testdata.push("11");
+    testdata.push(11);
+
+    
+    const [users, setUsers] = useState([
+        {
+          id: 1,
+          username: 'velopert',
+          email: 'public.velopert@gmail.com',
+          active : false,
+        },
+        {
+          id: 2,
+          username: 'tester',
+          email: 'tester@example.com',
+          active : false,
+        },
+        {
+          id: 3,
+          username: 'liz',
+          email: 'liz@example.com',
+          active : false,
+        }
+      ]) ;
+
+    const [inputs , setInputs] = useState({
+        username : '',
+        email : '' ,
+        active : false
+        
+    })
+
+    const activeCount : number = useMemo(()=> countActiveUsers(users) , [users]);
+
+    const {username , email} : any= inputs;
+
+    const onChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        const {name , value} = e.target;
+
+        setInputs({
+            ...inputs ,
+            [name] : value
+        })
+    }
+
+    const onRemove = (id : number) => {
+        setUsers(users.filter(item => {
+            return item.id !== id
+        }))
+    }
+
+    const onToggle = (id : number) => {
+        // console.log(users);
+
+        setUsers(
+            users.map(user =>
+              user.id === id ? { ...user, active: !user.active } : user
+            )
+          );
+    }
+        
+
+    const onCreate = () => {
+        
+        const user = {
+            id : nextId.current ++ ,
+            username ,
+            email , 
+            active : false
+        }
+        
+        setUsers([...users , user]);
+        
+    }
     return(
         <>
             <div>Test Array Sample</div>
+            <CreateUser onChange={onChange} onCreate={onCreate} />
             {users.map((item , index) => {
-                console.log(index);
+                //console.log(index);
 
-                return <User user={item} key={index}/>
+                return <User user={item} key={index} onRemove={onRemove} onToggle={onToggle}/>
             })}
             
-        
+            <div>
+                <p>활성화 사용자수 : {activeCount}</p>
+            </div>
         </>
     )
 }
 
-const User = ({user} : any) => {
+const User = ({user , onRemove , onToggle} : any) => {
+
     
+
+    useEffect(() => {
+        //console.log('user 값이 설정됨');
+        //console.log(user);
+        return () => {
+          //console.log('user 가 바뀌기 전..');
+          //console.log(user);
+        };
+      }, [user]);
     return(
     <div>
-
-        <b>{user.username}</b>
-        <span>{user.email}</span>
+        <span style={{padding : 10}}>{user.id}</span> 
+        <b style={{padding : 10 , cursor : 'pointer' , color : user.active ? 'green' : 'black'}} onClick={()=> onToggle(user.id)}>{user.username}</b>
+        <span style={{padding : 10}}>{user.email}</span>
+        <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
     )
+}
+
+//활성 사용자 수 계산
+
+const countActiveUsers  = (users : UserModel[]) => {
+    console.log("활성 사용자 수 계산");
+
+    return users.filter(user => user.active).length;
+    
+    
 }
 
 export default UserList;
